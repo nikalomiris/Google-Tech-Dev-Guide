69f2f523496e740a62df6276219664cd6d83f1e8
Nikos Kalomiris <nikalomiris@gmail.com>
2018-12-17 16:35:17 +0200

    Implements the solution of maxSpan problem.

---
+++ src/main/java/com/kalomiris/personal/foundationsOfProgramming/MaxSpan.java
@@ -0,0 +1,22 @@
+package com.kalomiris.personal.foundationsOfProgramming;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class MaxSpan {
+
+    public int getMaxSpan(List<Integer> list) {
+
+        List<Integer> results = new ArrayList<Integer>();
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = list.size() - 1; j >= i; j--) {
+                if (list.get(i) == list.get(j)) {
+                    results.add(j - i +1);
+                }
+            }
+        }
+        results.sort(Comparator.reverseOrder());
+        return  results.get(0);
+    }
+}
---
+++ src/test/java/com/kalomiris/personal/foundationsOfProgramming/MaxSpanTest.java
@@ -0,0 +1,46 @@
+package com.kalomiris.personal.foundationsOfProgramming;
+
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MaxSpanTest extends TestCase {
+    public void testGetMaxSpan() throws Exception {
+
+        MaxSpan maxSpan = new MaxSpan();
+        List<Integer> input1 = new ArrayList<Integer>();
+        input1.add(1);
+        input1.add(4);
+        input1.add(2);
+        input1.add(1);
+        input1.add(4);
+        input1.add(1);
+        input1.add(4);
+
+        List<Integer> input2 = new ArrayList<Integer>();
+        input2.add(1);
+        input2.add(2);
+        input2.add(1);
+        input2.add(1);
+        input2.add(3);
+
+        List<Integer> input3 = new ArrayList<Integer>();
+        input3.add(1);
+        input3.add(4);
+        input3.add(2);
+        input3.add(1);
+        input3.add(4);
+        input3.add(4);
+        input3.add(4);
+
+        List<Integer> input4 = new ArrayList<Integer>();
+        input4.add(1);
+
+        assertEquals(6, maxSpan.getMaxSpan(input1));
+        assertEquals(4, maxSpan.getMaxSpan(input2));
+        assertEquals(6, maxSpan.getMaxSpan(input3));
+        assertEquals(1, maxSpan.getMaxSpan(input4));
+    }
+
+}
